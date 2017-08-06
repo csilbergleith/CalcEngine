3714896dddf75dd23b5e8416ac3ecc0e7e37f038
csilbergleith <git@10ringaz.com>
2017-08-05 18:04:59 -0700

    Chapter Updates

--- src/com/csilberg/calcengine/Main.java
+++ src/com/csilberg/calcengine/Main.java
@@ -48,16 +48,23 @@ package com.csilberg.calcengine;
                         System.out.println(equationOverload.getResult());
 
                         System.out.println();
+                        System.out.println("Using Inheritance");
+
+                        CalculateBase[] calculators = {
+                                new Divider(100.0d, 50.0d),
+                                new Adder(25.0d,92.0d),
+                                new Subtracter(225.0d, 17.0d),
+                                new Multiplier(11.0d, 3.0d)
+                        };
+
+                        for(CalculateBase calculator: calculators ){
+                            calculator.calculate();
+                            System.out.print("result = ");
+                            System.out.println(calculator.getResult());
+                        }
+                        System.out.println();
                         System.out.println("---------End----------");
 
                 }
 
-//                    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-//                        MathEquation equation = new MathEquation(opCode,leftVal, rightVal );
-//                        equation.setLeftVal(leftVal);
-//                        equation.setRightVal(rightVal);
-//                        equation.setOpCode(opCode);
-//
-//                        return equation;
-//                    }
 }
